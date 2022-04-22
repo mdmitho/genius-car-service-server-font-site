e706d9f13ad880175070d836ac56e6a1f56c649d
Hossen <mdmitho199911@gmail.com>
2022-04-23 02:27:09 +0600

    manage services and  explore delete api

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Register from './Pages/Login/Register/Register';
 import Checkout from './Pages/Checkout/Checkout/Checkout';
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import AddService from './Pages/AddService/AddService';
+import ManageServices from './Pages/ManageServices/ManageServices';
 
 function App() {
   return (
@@ -42,6 +43,11 @@ function App() {
       <Route path='/addservice' element={
         <RequireAuth>
               <AddService></AddService> 
+        </RequireAuth>
+      }/>
+      <Route path='/manage' element={
+        <RequireAuth>
+              <ManageServices></ManageServices> 
         </RequireAuth>
       }/>
       <Route path='*' element={<NotFound></NotFound>}></Route>
---
+++ src/Pages/ManageServices/ManageServices.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import useServices from '../../Hooks/useServices'
+
+const ManageServices = () => {
+    const [services, setServices] = useServices()
+   const handleDelete = id =>{
+    const proceed = window.confirm('Are you sure?')
+    if(proceed){
+      const url = `http://localhost:5000/service/${id}`
+      fetch(url,{
+          method:'DELETE'
+      })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data)
+          const remaining = services.filter(service => service._id !== id)
+          setServices(remaining)
+      })
+    }
+   }
+    return (
+        <div className='w-50 mx-auto'>
+            <h1>Manage Your Service </h1>
+            {
+                services.map(service => <div key={service._id}>
+                    <h4>{service.name}<button onClick={()=>handleDelete(service._id)}>X</button></h4>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default ManageServices;
